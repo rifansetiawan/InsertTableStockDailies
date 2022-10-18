2adc993a4e6a663aa8f933be682b07e16ebf8549
rifan.setiawan <rifan.setiawan@paper.id>
2022-10-18 17:00:47 +0700

    add stock_daily python

--- stock_daily.py
+++ stock_daily.py
@@ -26,16 +26,16 @@ date_stock_yesterday = '2022-10-17 00:00:00'
 # print(stock_data_yesterday)
 # time.sleep(1000)
 f = open("stocks.txt", "w")
-sql = "INSERT INTO stock_dailies (datetime,code,open,high,low,last,volume, prev, stock_logo) VALUES"
+sql = "INSERT INTO stock_dailies (datetime,code,open,high,low,last,volume, prev, stock_logo,name) VALUES"
 val = "("
 for i in data:
     stock_data_yesterday = "(select close from stock_datas where stock_code = '" + i["Code"] +  "'" + "and date = '" + date_stock_yesterday + "')"
     stock_id = "(select uuid from stocks where code = '" + i["Code"] + "')"
     stock_data_yesterday_diff = "(" + str(i["Last"]) + " - (select close from stock_datas where stock_code = '" + i["Code"] +  "'" + "and date = '" + date_stock_yesterday + "')"
     stock_logo = "( select filename from stock_logos where stock_code = '" + i["Code"]  + "')"
-
+    stock_name = "( select name from stocks where code = '"  + i["Code"]  + "')"
     # print(type(i['Remarks']) )
-    data = "'"+str(i["DateTime"]).replace("T", " ") + "'" + "," + "'" + i["Code"] + "'" + ","  +str(i["Open"])  + "," + str(i["High"]) + ","+ str(i["Low"])  + "," + str(i["Last"]) + ","+ str( i["Volume"]) + "," + stock_data_yesterday + ","+stock_logo
+    data = "'"+str(i["DateTime"]).replace("T", " ") + "'" + "," + "'" + i["Code"] + "'" + ","  +str(i["Open"])  + "," + str(i["High"]) + ","+ str(i["Low"])  + "," + str(i["Last"]) + ","+ str( i["Volume"]) + "," + stock_data_yesterday + ","+stock_logo + ","+ stock_name
     command_val = "(" + data + ")" + ";"
     print(data)
     print(sql + command_val)
