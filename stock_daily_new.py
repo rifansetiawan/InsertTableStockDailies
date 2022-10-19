3a26a76949f28433d80871a6333dfe4c6ad69e6a
rifan.setiawan <rifan.setiawan@paper.id>
2022-10-19 12:51:13 +0700

    edit

--- stock_daily_new.py
+++ stock_daily_new.py
@@ -44,7 +44,7 @@ mycursor.execute("DELETE FROM stock_dailies")
 sql = "INSERT INTO stock_dailies (datetime,code,open,high,low,last,volume, prev, stock_logo,name, uuid) VALUES"
 val = "("
 for i in df_agg.itertuples():
-    dateeeTime = datetime.strptime(getattr(i, 'Date/Time'), "%m/%d/%Y %H:%M:%S").strftime('%Y-%m-%d %H:%M:%S')
+    dateeeTime = datetime.strptime(i[df.columns.get_loc('Date/Time')], "%m/%d/%Y %H:%M:%S").strftime('%Y-%m-%d %H:%M:%S')
     # rrrrr = dateeeTime.strftime("%Y-%m-%d %H:%M:%S")
     # dateeeTime = dateeeTime
     print(dateeeTime)
