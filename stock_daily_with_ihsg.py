c31af14bca9e58da3434e5b89bb1f499ca9527e3
rifan.setiawan <rifan.setiawan@paper.id>
2022-10-27 10:48:26 +0700

    edit

--- stock_daily_with_ihsg.py
+++ stock_daily_with_ihsg.py
@@ -49,7 +49,7 @@ for i in df_agg.itertuples():
     # dateeeTime = dateeeTime
     print(dateeeTime)
     # time.sleep(10000)
-    if i.Code == 'COMPOSITE' :
+    if i.Index == 'COMPOSITE' :
         stock_data_yesterday = "(select close from stock_daily_historicals where stock_code = '" + i.Index +  "'" + "ORDER BY DATE DESC LIMIT 1" +  ")"
         stock_id = "(select uuid from stocks where code = '" + i.Index + "')"
         stock_data_yesterday_diff = "(" + str(i.Close) + " - (select close from stock_datas where stock_code = '" + i.Index +  "'" + "and date = '" + date_stock_yesterday + "')"
