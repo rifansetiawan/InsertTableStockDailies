67f872582f73cf998e835d01626d8175cdd33eee
rifan.setiawan <rifan.setiawan@paper.id>
2022-11-03 18:26:12 +0700

    add nov

--- stock_daily_historical.py
+++ stock_daily_historical.py
@@ -36,15 +36,14 @@ while True:
 # print(df_agg)
 df = pd.read_csv(dst)
 
-df_agg=df
+df_agg=df.groupby("Ticker").last()
 
 date_stock_yesterday = '2022-10-21 00:00:00'
 
 # mycursor.execute("DELETE FROM stock_dailies")
-
 sql = "INSERT INTO stock_daily_historicals (datetime,code,open,high,low,last,volume) VALUES"
 val = "("
-for i in df_agg:
+for i in df_agg.itertuples():
     dateeeTime = datetime.strptime(i[df.columns.get_loc('Date/Time')], "%m/%d/%Y %H:%M:%S").strftime('%Y-%m-%d %H:%M:%S')
     # rrrrr = dateeeTime.strftime("%Y-%m-%d %H:%M:%S")
     # dateeeTime = dateeeTime
