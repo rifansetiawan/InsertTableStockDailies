2a0fc2bad09a3e910647368d8da8849088b8a991
rifan.setiawan <rifan.setiawan@paper.id>
2022-11-06 21:57:09 +0700

    update code

--- stock_daily_historical.py
+++ stock_daily_historical.py
@@ -50,7 +50,7 @@ with open(dst) as f:
     for row in cf:
         print(row)
         print(row[1][0:-9])
-        if row[1][0:-9] != date_now:
+        if row[1][0:-9] != date_now and row != '':
             data_temp.append(row)
         
 with open('all_historical_latest.csv', 'w', encoding='UTF8', newline='') as f_tmp:
