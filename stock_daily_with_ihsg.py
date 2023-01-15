e8068a1798e5917bd85303da560a7e0bf524fecd
rifan.setiawan <rifan.setiawan@paper.id>
2023-01-15 21:40:25 +0700

    update

--- stock_daily_with_ihsg.py
+++ stock_daily_with_ihsg.py
@@ -77,8 +77,7 @@ for i in df_agg.itertuples():
 
     # stock_highest_df = df.groupby("Ticker")[""]
     # stock_lowest_df = df.groupby("Ticker")[""]
-    stock_prev = df_januari.groupby("Ticker").get_group(str(i.Index)).last()["Last"]
-
+    stock_prev = df_januari.groupby("Ticker").get_group(str(i.Index)).last()
     print(stock_prev)
     stock_volume_df = df.groupby("Ticker").get_group(str(i.Index))["Volume"]
     stock_high_df = df.groupby("Ticker").get_group(str(i.Index))["High"]
